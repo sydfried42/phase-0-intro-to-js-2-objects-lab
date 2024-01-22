a488f56e65e017f71a256dd77d858d8b4eab3b42
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-21 23:05:10 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {
+      ...employee,
+      name: 'Sam',
+      streetAddress: '11 Broadway',
+    };
+  
+    return updatedEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
